18e385e224862f316fe0749aa56c97054947b13d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-24 11:47:15 +0900

    Create function that array total of 35 dates

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { getCalendarColumns } from "./src/helper/util";
+import dayjs from "dayjs";
+import { useEffect } from "react";
 
 export default function App() {
+  const now = dayjs();
+  const colums = getCalendarColumns(now);
+
+  useEffect(() => {}, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
